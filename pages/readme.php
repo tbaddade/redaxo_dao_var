92f93419bea6e84e17ddf0bc082f6e11cea332a4
tbaddade <thomas@redaxo.org>
2016-02-11 22:41:28 +0100

    Hilfeseite angepasst

--- pages/readme.php
+++ pages/readme.php
@@ -23,7 +23,12 @@ $vars = [
         'var' => 'REX_DAO_SELECT[]',
         'inp' => [
             'REX_DAO_SELECT[id="1" widget="1" options="Please select,H1=h1,H2=h2"]',
-            'REX_DAO_SELECT[id="1" widget="1" options="Please select|optGroup A:H1=h1,H2=h2|optGroup B:H3=h3,H4=h4"]'],
+            'REX_DAO_SELECT[id="1" widget="1" options="Please select|optGroupA:Heading 1=h1,Heading 2=h2|optGroupB:Heading 3=h3,Heading 4=h4"]',
+            'REX_DAO_SELECT[id="1" widget="1" options="Please select|America:Canada=ca,USA=us|Europe:Germany=de,Danmark=dk" output="form:3:9" label="Country"]',
+            'REX_DAO_SELECT[id="1" widget="1" options="SELECT name, id FROM rex_article ORDER BY name" output="form:3:9" label="Select an article"]',
+            'REX_DAO_SELECT[id="1" widget="1" options="German articles:SELECT name, id FROM rex_article WHERE clang_id = 1 ORDER BY name|English articles:SELECT name, id FROM rex_article WHERE clang_id = 2 ORDER BY name" output="form:3:9" label="Select an article"]',
+            'options ' . "\n" . ' > key=value Pair > Comma "," separated ' . "\n" . ' > optgroups > Pipe "|" separated ' . "\n" . ' > optgroup label > colon ":" separated from the pairs',
+        ],
         'out' => ['REX_DAO_SELECT[id="1"]',],
     ], [
         'var' => 'REX_DAO_CATEGORY_SELECT[]',
@@ -31,6 +36,13 @@ $vars = [
             'REX_DAO_CATEGORY_SELECT[id="1" widget="1"]',
             'REX_DAO_CATEGORY_SELECT[id="1" widget="1" root="5"]'],
         'out' => ['REX_DAO_CATEGORY_SELECT[id="1"]',],
+    ], [
+        'var' => 'REX_DAO_VALUE[]',
+        'inp' => [
+            'REX_DAO_VALUE[id="1" widget="1"]',
+            'REX_DAO_VALUE[id="1" widget="1" type="email"]',
+            'REX_DAO_VALUE[id="1" widget="1" type="textarea"]'],
+        'out' => ['REX_DAO_VALUE[id="1"]',],
     ]
 ];
 
@@ -43,14 +55,14 @@ foreach ($vars as $var) {
 
     $rows .= '<dl class="dl-horizontal text-left"><dt>Eingabe:</dt>';
     foreach ($var['inp'] as $value) {
-        $rows .= '<dd><p><b><code>' . htmlspecialchars($value) . '</code></b></p></dd>';
+        $rows .= '<dd><p><b><code>' . nl2br(htmlspecialchars($value)) . '</code></b></p></dd>';
     }
     $rows .= '</dl>';
 
     $rows .= '<dl class="dl-horizontal text-left"><dt>Ausgabe:</dt>';
     foreach ($var['out'] as $key => $value) {
         if (is_int($key)) {
-            $rows .= '<dd><b><code>' . htmlspecialchars($value) . '</code></b></dd>';
+            $rows .= '<dd><b><code>' . nl2br(htmlspecialchars($value)) . '</code></b></dd>';
         } else {
             $rows .= '<dd><p><b><code>' . htmlspecialchars($key) . '</code></b><br /><i class="fa fa-hand-o-right"></i>&nbsp;&nbsp;<code>' . htmlspecialchars($value) . '</code></p></dd>';
         }
