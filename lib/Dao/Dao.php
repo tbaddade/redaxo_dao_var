80c9e7a8e40655eff41ad3785e2e3c1fede0474f
tbaddade <thomas@redaxo.org>
2016-05-03 22:41:00 +0200

    Forumlaranpassungen

--- lib/Dao/Dao.php
+++ lib/Dao/Dao.php
@@ -13,20 +13,24 @@ class Dao
 {
     public static function getForm($field, $label, $output)
     {
-        $labelClass = 'col-md-2';
-        $fieldClass = ($label == '') ? 'col-md-12' : 'col-md-10';
-
-        $output = explode(':', $output);
-        if (count($output) == 3) {
-            $labelClass = (is_numeric($output[1])) ? 'col-md-' . $output[1] : $output[1];
-            $fieldClass = (is_numeric($output[2])) ? 'col-md-' . $output[2] : $output[2];
-        }
-
         $fragment = new rex_fragment();
         $fragment->setVar('label', $label, false);
         $fragment->setVar('field', $field, false);
-        $fragment->setVar('labelClass', $labelClass, false);
-        $fragment->setVar('fieldClass', $fieldClass, false);
+
+        $output = explode(':', $output);
+        if ($output[0] == 'form') {
+            $labelClass = 'col-md-2';
+            $fieldClass = ($label == '') ? 'col-md-12' : 'col-md-10';
+
+            if (count($output) == 3) {
+                $labelClass = (is_numeric($output[1])) ? 'col-md-' . $output[1] : $output[1];
+                $fieldClass = (is_numeric($output[2])) ? 'col-md-' . $output[2] : $output[2];
+            }
+
+            $fragment->setVar('labelClass', $labelClass, false);
+            $fragment->setVar('fieldClass', $fieldClass, false);
+        }
+
         return $fragment->parse('dao/form.php');
     }
 }
