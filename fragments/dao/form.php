630b8845f740422acd06643a52a9a054f08b89bc
tbaddade <thomas@redaxo.org>
2016-07-14 09:40:48 +0200

    Leerzeichen hinzugefügt

--- fragments/dao/form.php
+++ fragments/dao/form.php
@@ -2,7 +2,7 @@
     <?php if (isset($this->label) && $this->label != ''): ?>
         <label class="<?= (isset($this->labelClass) && $this->labelClass != '') ? trim($this->labelClass) . ' ' : '' ?>control-label"><?= $this->label ?></label>
     <?php endif; ?>
-    <div<?= (isset($this->fieldClass) && $this->fieldClass != '' ? 'class="' . $this->fieldClass . '"' : '')  ?>>
+    <div<?= (isset($this->fieldClass) && $this->fieldClass != '' ? ' class="' . $this->fieldClass . '"' : '')  ?>>
         <?= $this->field ?>
     </div>
 </div>
